abb47065f3e174fc22324904a9028925b199bfe7
Eubene Sa <eubene.sa@gmail.com>
2016-07-25 20:21:59 -0700

    (ex2) prototype-based NotesManager, needs refactor

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,144 +1,119 @@
-var NotesManager = (function(){
+function NotesManager() {
+  this.notes = [];
+}
 
-	function addNote(note) {
-		$notes.prepend(
-			$("<a href='#'></a>")
-			.addClass("note")
-			.text(note)
-		);
-	}
+NotesManager.prototype.addNote = function(note) {
+  this.$notes.prepend($('<a href=\'#\'></a>').addClass('note').text(note));
+};
 
-	function addCurrentNote() {
-		var current_note = $new_note.val();
+NotesManager.prototype.addCurrentNote = function() {
+  var currentNote = this.$newNote.val();
 
-		if (current_note) {
-			notes.push(current_note);
-			addNote(current_note);
-			$new_note.val("");
-		}
-	}
+  if (currentNote) {
+    this.notes.push(currentNote);
+    this.addNote(currentNote);
+    this.$newNote.val('');
+  }
+};
 
-	function showHelp() {
-		$help.show();
+NotesManager.prototype.showHelp = function() {
+  var self = this;
+  self.$help.show();
 
-		document.addEventListener("click",function __handler__(evt){
-			document.removeEventListener("click",__handler__,true);
-			hideHelp();
-			evt.preventDefault();
-			evt.stopPropagation();
-			evt.stopImmediatePropagation();
-		},true);
-	}
+  document.addEventListener('click', function __handler__(e) {
+    document.removeEventListener('click', __handler__, true);
+    self.hideHelp();
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+  }, true);
+};
 
-	function hideHelp() {
-		$help.hide();
-	}
+NotesManager.prototype.hideHelp = function() {
+  this.$help.hide();
+};
 
-	function loadData(data) {
-		for (var i=0; i<data.length; i++) {
-			notes.push(data[i]);
-		}
-	}
+NotesManager.prototype.loadData = function(data) {
+  this.notes = this.notes.concat(data);
+};
 
-	function handleOpenHelp(evt) {
-		if (!$help.is(":visible")) {
-			evt.stopPropagation();
-			showHelp();
-		}
-	}
+NotesManager.prototype.handleOpenHelp = function(e) {
+  if (!this.$help.is(':visible')) {
+    e.stopPropagation();
+    this.showHelp();
+  }
+};
 
-	function handleAddNote(evt) {
-		addCurrentNote();
-	}
+NotesManager.prototype.handleAddNote = function(e) {
+  this.addCurrentNote();
+};
 
-	function handleEnter(evt) {
-		if (evt.which == 13) {
-			addCurrentNote();
-		}
-	}
+NotesManager.prototype.handleEnter = function(e) {
+  if (e.which === 13) {
+    this.addCurrentNote();
+  }
+};
 
-	function handleDocumentClick(evt) {
-		$notes.removeClass("active");
-		$notes.children(".note").removeClass("highlighted");
-	}
+NotesManager.prototype.handleDocumentClick = function(e) {
+  this.$notes.removeClass('active');
+  this.$notes.children('.note').removeClass('highlighted');
+};
 
-	function handleNoteClick(evt) {
-		evt.preventDefault();
-		evt.stopPropagation();
+NotesManager.prototype.handleNoteClick = function(e) {
+  e.preventDefault();
+  e.stopPropagation();
 
-		$notes.addClass("active");
-		$notes.children(".note").removeClass("highlighted");
-		$(evt.target).addClass("highlighted");
-	}
+  this.$notes.addClass('active');
+  this.$notes.children('.note').removeClass('highlighted');
+  $(e.target).addClass('highlighted');
+};
 
-	function init(opts) {
-		// cache references to the DOM elements we need to manage
-		$notes = $(opts.notes);
-		$new_note = $(opts.new_note);
-		$add_note = $(opts.add_note);
-		$help = $(opts.help);
-		$open_help = $(opts.open_help);
+NotesManager.prototype.init = function(opts) {
+  // cache references to the DOM elements we need to manage
+  this.$notes = $(opts.notes);
+  this.$newNote = $(opts.newNote);
+  this.$addNote = $(opts.addNote);
+  this.$help = $(opts.help);
+  this.$openHelp = $(opts.openHelp);
 
-		// build the initial list from the existing `notes` data
-		var html = "";
-		for (i=0; i<notes.length; i++) {
-			html += "<a href='#' class='note'>" + notes[i] + "</a>";
-		}
-		$notes.html(html);
+  // build the initial list from the existing `notes` data
+  var html = '';
+  for (let i = 0; i < this.notes.length; i++) {
+    html += `<a href=\'#\' class=\'note\'>${this.notes[i]}</a>`;
+  }
+  this.$notes.html(html);
 
-		// listen to "help" button
-		$open_help.bind("click",handleOpenHelp);
+  // listen to "help" button
+  this.$openHelp.bind('click', this.handleOpenHelp.bind(this));
 
-		// listen to "add" button
-		$add_note.bind("click",handleAddNote);
+  // listen to "add" button
+  this.$addNote.bind('click', this.handleAddNote.bind(this));
 
-		// listen for <enter> in text box
-		$new_note.bind("keypress",handleEnter);
+  // listen for <enter> in text box
+  this.$newNote.bind('keypress', this.handleEnter.bind(this));
 
-		// listen for clicks outside the notes box
-		$(document).bind("click",handleDocumentClick);
+  // listen for clicks outside the notes box
+  $(document).bind('click', this.handleDocumentClick.bind(this));
 
-		// listen for clicks on note elements
-		$notes.on("click",".note",handleNoteClick);
-	}
+  // listen for clicks on note elements
+  this.$notes.on('click', '.note', this.handleNoteClick.bind(this));
+};
 
-
-	var
-		// private `notes` data
-		notes = [],
-
-		// DOM refs
-		$notes,
-		$new_note,
-		$add_note,
-		$help,
-		$open_help,
-
-		// module API
-		publicAPI = {
-			loadData: loadData,
-			init: init
-		}
-	;
-
-	return publicAPI;
-})();
-
+var myNotes = new NotesManager();
 
 // assume this data came from the database
-NotesManager.loadData([
-	"This is the first note I've taken!",
-	"Now is the time for all good men to come to the aid of their country.",
-	"The quick brown fox jumped over the moon."
+myNotes.loadData([
+  'This is the first note I\'ve taken!',
+  'Now is the time for all good men to come to the aid of their country.',
+  'The quick brown fox jumped over the moon.'
 ]);
 
-
 $(document).ready(function(){
-	NotesManager.init({
-		notes: "#notes",
-		new_note: "#note",
-		add_note: "#add_note",
-		help: "#help",
-		open_help: "#open_help"
-	});
+  myNotes.init({
+    notes: '#notes',
+    newNote: '#note',
+    addNote: '#add_note',
+    help: '#help',
+    openHelp: '#open_help'
+  });
 });
