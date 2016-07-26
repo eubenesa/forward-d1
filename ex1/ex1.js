7fd5adef9a3263913268f353ecaec9d84eed1d6d
Eubene Sa <eubene.sa@gmail.com>
2016-07-25 18:53:43 -0700

    (ex1) Cleanup

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -1,87 +1,88 @@
 var NotesManager = (function() {
+  var notes = [];
+
   function addNote(note) {
-    $("#notes").prepend(
-      $("<a href='#'></a>")
-      .addClass("note")
+    $('#notes').prepend(
+      $('<a href=\'#\'></a>')
+      .addClass('note')
       .text(note)
     );
   }
 
   function addCurrentNote() {
-    var current_note = $("#note").val();
+    var currentNote = $('#note').val();
 
-    if (current_note) {
-      notes.push(current_note);
-      addNote(current_note);
-      $("#note").val("");
+    if (currentNote) {
+      notes.push(currentNote);
+      addNote(currentNote);
+      $('#note').val('');
     }
   }
 
   function showHelp() {
-    $("#help").show();
+    $('#help').show();
 
-    document.addEventListener("click",function __handler__(evt){
-      document.removeEventListener("click",__handler__,true);
+    document.addEventListener('click', function __handler__(e) {
+      document.removeEventListener('click', __handler__, true);
       hideHelp();
-      evt.preventDefault();
-      evt.stopPropagation();
-      evt.stopImmediatePropagation();
-    },true);
+
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+    }, true);
   }
 
   function hideHelp() {
-    $("#help").hide();
+    $('#help').hide();
   }
 
-  var notes = [];
   function loadNotes(data) {
     notes = notes.concat(data);
-    // console.log(notes);
   }
 
   function init() {
-    var $notes = $("#notes");
+    var $notes = $('#notes');
 
     // build the list from the "database"
-    var html = "";
-    for (i=0; i<notes.length; i++) {
-      html += "<a href='#' class='note'>" + notes[i] + "</a>";
+    var html = '';
+    for (let i = 0; i < notes.length; i++) {
+      html += `<a href=\'#\' class=\'note\'>${notes[i]}</a>`;
     }
     $notes.html(html);
 
     // listen to "help" button
-    $("#open_help").bind("click",function(evt){
-      if (!$("#help").is(":visible")) {
+    $('#open_help').bind('click', function(e) {
+      if (!$('#help').is(':visible')) {
         showHelp();
       }
     });
 
     // listen to "add" button
-    $("#add_note").bind("click",function(evt){
+    $('#add_note').bind('click', function(e) {
       addCurrentNote();
     });
 
     // listen for <enter> in text box
-    $("#note").bind("keypress",function(evt){
-      if (evt.which == 13) {
+    $('#note').bind('keypress', function(e) {
+      if (e.which === 13) {
         addCurrentNote();
       }
     });
 
     // listen for clicks outside the notes box
-    $(document).bind("click",function(evt){
-      $notes.removeClass("active");
-      $notes.children(".note").removeClass("highlighted");
+    $(document).bind('click', function(e) {
+      $notes.removeClass('active');
+      $notes.children('.note').removeClass('highlighted');
     });
 
     // listen for clicks on note elements
-    $notes.on("click",".note",function(evt){
-      $notes.addClass("active");
-      $notes.children(".note").removeClass("highlighted");
-      $(evt.target).addClass("highlighted");
+    $notes.on('click', '.note', function(e) {
+      $notes.addClass('active');
+      $notes.children('.note').removeClass('highlighted');
+      $(e.target).addClass('highlighted');
 
-      evt.preventDefault();
-      evt.stopPropagation();
+      e.preventDefault();
+      e.stopPropagation();
     });
   }
 
@@ -94,9 +95,9 @@ var NotesManager = (function() {
 })();
 
 NotesManager.loadNotes([
-  "This is the first note I've taken!",
-  "Now is the time for all good men to come to the aid of their country.",
-  "The quick brown fox jumped over the moon."
+  'This is the first note I\'ve taken!',
+  'Now is the time for all good men to come to the aid of their country.',
+  'The quick brown fox jumped over the moon.'
 ]);
 
 $(document).ready(function() {
